5760b27bffbd14da01b965bfa1156f49e7342b91
Unknown <maxscaddie@gmail.com>
2020-12-19 15:38:05 +1100

    Combined react and flask

--- voting-app/src/App.tsx
+++ voting-app/src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-          <p>Landing page</p>
+        <p>The current time is {currentTime}.</p>
       </header>
     </div>
   );
