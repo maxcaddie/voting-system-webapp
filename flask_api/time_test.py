a1005c8ec13b0221d08beace0a4a0f662c5363a2
Unknown <maxscaddie@gmail.com>
2020-12-23 16:38:36 +1100

    Added the ability to get from firebase

--- flask_api/time_test.py
+++ flask_api/time_test.py
@@ -1,8 +1,26 @@
 import time
 from flask import Flask
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("key.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
 
 app = Flask(__name__)
+
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/get')
+def get():
+    users_ref = db.collection(u'users')
+    docs = users_ref.stream()
+    users_database_dict = {}
+    for doc in docs:
+        users_database_dict[doc.id] = doc.to_dict()
+    print(users_database_dict)
+    return users_database_dict
--- voting-app/src/App.tsx
+++ voting-app/src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     fetch('/time').then(res => res.json()).then(data => {
       setCurrentTime(data.time);
     });
+    fetch('/get');
   }, []);
 
   return (
