ee3e17250e581a6cbc56913949568b0fcfc14b13
Unknown <maxscaddie@gmail.com>
2020-12-27 16:00:47 +1100

    Add linter

--- voting-app/src/App.tsx
+++ voting-app/src/App.tsx
@@ -3,9 +3,8 @@ import React, { useState, useEffect } from 'react';
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
 
-
-  function updateTime(){
-    fetch('/time').then(res => res.json()).then(data => {
+  function updateTime() {
+    fetch('/time').then((res) => res.json()).then((data) => {
       setCurrentTime(data.time);
     });
   }
@@ -17,9 +16,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>The current time is {currentTime}.</p>
+        <p>
+          The current time is
+          {currentTime}
+          .
+        </p>
       </header>
-      <form action="http://localhost:5000/uploader" method = "POST" encType="multipart/form-data">
+      <form action="http://localhost:5000/uploader" method="POST" encType="multipart/form-data">
         <input type="file" name="file" />
         <input type="submit" />
       </form>
