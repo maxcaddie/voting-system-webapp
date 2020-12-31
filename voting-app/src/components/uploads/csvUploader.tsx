5aad303b2e3de43187142a819f41c07715294243
Unknown <maxscaddie@gmail.com>
2020-12-31 17:14:27 +1100

    changed upload link to include userid

--- voting-app/src/components/uploads/csvUploader.tsx
+++ voting-app/src/components/uploads/csvUploader.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { localStorageUserID } from '../../constants/authenticationConstants';
 
 function CsvUploader() {
+  const getUserId = (() => localStorage.getItem(localStorageUserID));
+
   return (
-    <form action="/uploader" method="POST" encType="multipart/form-data">
+    <form action={`/uploader/${getUserId()}`} method="POST" encType="multipart/form-data">
       <input type="file" name="file" />
       <input type="submit" />
     </form>
