37b7982c6354d227b8d3ce90e6805a38d305965d
Main <maxscaddie@gmail.com>
2021-01-31 20:09:03 +1100

    Apparently exposing the key is fine

---
+++ voting-app/src/firebaseapikey.tsx
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAVepinAZxtpba_e4WJ31R8vENjsHGKn8g',
+  authDomain: 'voting-app-219d5.firebaseapp.com',
+  projectId: 'voting-app-219d5',
+  storageBucket: 'voting-app-219d5.appspot.com',
+  messagingSenderId: '141904924688',
+  appId: '1:141904924688:web:35607d926a94e761d13a95',
+  measurementId: 'G-NPWNFN3R84',
+};
+
+export default firebaseConfig;
